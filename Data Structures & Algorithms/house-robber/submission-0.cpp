6461c93939a817a9bfa07705cb860e598e240464
Dhruvdj2004 <dhruvdj1971@gmail.com>
2026-06-22 01:10:08 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int rob(vector<int>& cost) {
+        
+        int n = cost.size();
+         vector<int>dp(n,0);
+
+        if(n == 1)
+            return cost[0];
+
+        if(n == 2)
+            return max(cost[0], cost[1]);
+            dp[0]=cost[0];
+            dp[1]=max(cost[0], cost[1]);
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = max((cost[i]+dp[i-2]),dp[i-1]);
+        }
+
+        return dp[n-1];
+    
+    }
+};
