71ef873c86008e513d15697e37363966cffcff05
Dhruvdj2004 <dhruvdj1971@gmail.com>
2026-07-13 17:49:28 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+    bool board2(vector<vector<char>>& board, string word, int i, int j, int k) {
+
+        if (k == word.size())
+            return true;
+
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size())
+            return false;
+
+        if (board[i][j] != word[k])
+            return false;
+
+        char temp = board[i][j];
+        board[i][j] = '#';   // visited
+
+        if (board2(board, word, i + 1, j, k + 1))
+            return true;
+
+        if (board2(board, word, i - 1, j, k + 1))
+            return true;
+
+        if (board2(board, word, i, j + 1, k + 1))
+            return true;
+
+        if (board2(board, word, i, j - 1, k + 1))
+            return true;
+
+        board[i][j] = temp;   // backtrack
+
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+
+                if (board2(board, word, i, j, 0))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};
